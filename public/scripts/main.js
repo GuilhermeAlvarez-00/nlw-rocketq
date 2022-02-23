3e09abfac34ab3e9ffe3a2ea3226e54c600253d7
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-02-23 13:46:48 -0300

    configurando modal dinamico no ejs e node

---
+++ public/scripts/main.js
@@ -0,0 +1,42 @@
+import { Modal } from './modal.js'
+
+const modal = Modal()
+
+const html = {
+  get(element) {
+    return document.querySelector(element)
+  },
+  content(element, content) {
+    return (element.innerHTML = content)
+  }
+}
+
+const modalTitle = html.get('.modal h2')
+const modalDescription = html.get('.modal p')
+const modalButton = html.get('.modal button')
+
+const checkButtons = document.querySelectorAll('.actions a.check')
+checkButtons.forEach(button => {
+  button.addEventListener('click', handleClick)
+})
+
+const deleteButtons = document.querySelectorAll('.actions .delete')
+deleteButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    handleClick(event, false)
+  })
+})
+
+function handleClick(event, check = true) {
+  event.preventDefault()
+
+  const text = check ? 'Marcar como lida' : 'Excluir'
+  const descriptionTitle = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+
+  html.content(modalTitle, check ? `${text}` : `${text} pergunta`)
+  html.content(modalDescription, descriptionTitle)
+  html.content(modalButton, check ? 'Sim, confirmar' : 'Sim, excluir')
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+  modal.open()
+}
