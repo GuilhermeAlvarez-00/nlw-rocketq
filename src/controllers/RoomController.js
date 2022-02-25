b1c3fc22efac03e5f5d69851b3c705b07f849e28
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-02-25 00:53:38 -0300

    configurando controller roomController para salvar dados no banco

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,23 @@
+import { openDb } from '../db/config.js'
+
 export default {
-  create(req, res) {
-    let roomId = 12345
+  async create(req, res) {
+    const { password } = req.body
+
+    const db = await openDb()
+
+    let roomId = 0
+    for (let i = 0; i < 6; i++) {
+      roomId === 0
+        ? (roomId = Math.floor(Math.random() * 10))
+        : (roomId += Math.floor(Math.random() * 10).toString())
+    }
+
+    await db.run(
+      `INSERT INTO rooms (id, pass) VALUES (${Number(roomId)}, ${password})`
+    )
+
+    await db.close()
 
     res.redirect(`room/${roomId}`)
   }
